2780d278714897f16b09db1c7126ce234ac75adf
Elvonia <60554110+Elvonia@users.noreply.github.com>
2024-05-24 11:38:04 -0400

    Add files via upload

---
+++ DeathSkipMod.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using MelonLoader;
+using PoY_DeathSkip;
+
+[assembly: MelonInfo(typeof(DeathSkipMod), "Death Skip", "1.0", "Kalico")]
+[assembly: MelonGame("TraipseWare", "Peaks of Yore")]
+
+namespace PoY_DeathSkip
+{
+    public class DeathSkipMod : MelonMod
+    {
+        [HarmonyPatch(typeof(FallingEvent), "FellToDeath")]
+        private static class DeathPatch
+        {
+            private static bool Prefix()
+            {
+                if (GameManager.control.permaDeathEnabled || GameManager.control.freesoloEnabled)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+    }
+}
